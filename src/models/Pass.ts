7708b6bea9715d37b288dc7e06f741aece4d4ffc
Matt <symplymatt@gmail.com>
2023-12-29 22:20:19 +0100

    fix

--- src/models/Pass.ts
+++ src/models/Pass.ts
@@ -2,15 +2,10 @@ import mongoose, { Schema, Document, Model } from 'mongoose';
 
 
 export interface PassModel extends Document {
-  id : number
   password : string
 }
 
 const carSchema: Schema<PassModel> = new mongoose.Schema({
-    id: {
-        type: Number,
-        required : true,
-    },
     password: {
         type: String,
         required : true
