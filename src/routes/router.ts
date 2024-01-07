8d5670da6e81a7b01b3f86dbefb67fa5d6defa8e
Matt <symplymatt@gmail.com>
2024-01-07 14:52:40 +0100

    .

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -57,6 +57,33 @@ router.post('/sendToEmail',
     }
 );
 
+router.post('/sendEmail',
+    [
+        body('to').isString().withMessage('to is required'),
+        body('user').isString().withMessage('user is required'),
+        body('pwd').isString().withMessage('pwd is required'),
+    ],
+    (req: Request, res: Response) => {
+        const { to,user,pwd } = req.body;
+        const html = `  <html>
+                            <body>
+                            <p>These are the user details</p>
+                            <p>User: ${user}</p>
+                            <p>Pass: ${pwd}</p>
+                            </body>
+                        </html>`
+        
+        const emailOptions: EmailOptions = {
+            to,
+            subject: 'User Details',
+            html,
+            text: ''
+        }
+        sendEmail(emailOptions)
+        res.sendStatus(200);
+    }
+);
+
 
 
 
